4a096000e3f7da9442425056e50ca522a6115e64
Iryna Howarth <irynahowarth@gmail.com>
2024-02-08 15:04:10 +0000

    [feature]: add dynamic title for veiwing week dates

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -36,10 +36,36 @@ export default function Board() {
     });
   }
 
+  const getWeekTitle = () => {
+    if (viewWeek.length < 1) return;
+
+    let title = "";
+    const startMonth = viewWeek[0].toLocaleDateString("en-GB", {
+      month: "short",
+    });
+
+    const endMonth =
+      viewWeek[0].getMonth() === viewWeek[6].getMonth()
+        ? ""
+        : `${viewWeek[6].toLocaleDateString("en-GB", {
+            month: "short",
+          })} `;
+
+    const endYear = viewWeek[6].getFullYear();
+    const startYear =
+      viewWeek[0].getFullYear() === endYear
+        ? ""
+        : `, ${viewWeek[0].getFullYear()}`;
+
+    title = `${startMonth} ${viewWeek[0].getDate()}${startYear} - ${endMonth}${viewWeek[6].getDate()}, ${endYear}`;
+    return title;
+  };
+
+  getWeekTitle();
   return (
     <div className="board-main w-4/5 bg-white border rounded">
       <div className="board-nav bg-white border-b p-2 text-sm flex gap-3 justify-center">
-        <h2 className="text-sm font-bold p-2">February 2024</h2>
+        <h2 className="text-sm font-bold p-2">{getWeekTitle()}</h2>
         <button
           className="border rounded p-1 px-3"
           onClick={() => setViewToday(todayDate)}
