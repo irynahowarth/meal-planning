840e4c597c1823a237c9dd750e2c9d2e54f1304f
Iryna Howarth <irynahowarth@gmail.com>
2024-02-14 19:25:35 +0000

    [feature]: ViewRecordModal: add side DropMenu to edit and delete screens

---
+++ src/components/Layout/DropMenu.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import * as Select from "@radix-ui/react-select";
+import DropDotsIcon from "../icons/DropDotsIcon";
+
+export default function DropMenu() {
+  const [value, setValue] = React.useState(null);
+
+  if (value === "edit") console.log("edit");
+  if (value === "delete") console.log("delete");
+
+  return (
+    <Select.Root value={value} onValueChange={setValue}>
+      <Select.Trigger>
+        <Select.Icon className="">
+          <DropDotsIcon />
+        </Select.Icon>
+      </Select.Trigger>
+
+      <Select.Content
+        position="popper"
+        align="end"
+        sideOffset={15}
+        className=" z-40 min-w-[80px] bg-gray-600 text-white text-sm rounded-md p-2.5 data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade cursor-pointer"
+      >
+        <Select.Group className="flex flex-col justify-between gap-2">
+          <Select.Item value="edit">
+            <Select.ItemText>Edit </Select.ItemText>
+          </Select.Item>
+          <Select.Item value="delete">
+            <Select.ItemText>Delete</Select.ItemText>
+          </Select.Item>
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  );
+}
--- src/components/Modal/Record/ViewRecordModal.jsx
+++ src/components/Modal/Record/ViewRecordModal.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import DropMenu from "../../Layout/DropMenu";
 
 export default function ViewRecordModal({ modalData }) {
   return (
     <>
-      <div className=" bg-gray-50 font-semibold text-md text-800 p-4 border-b">
-        View Meal Record (id:{modalData.id})
+      <div className="flex justify-between  bg-gray-50 font-semibold text-md text-800 p-4 border-b overflow-visible">
+        <h3>View Meal Record (id:{modalData.id})</h3>
+        <DropMenu />
       </div>
       <div className="p-6 flex flex-col gap-2">
         <div>{modalData.name}</div>
