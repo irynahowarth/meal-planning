44060342d64cd499fb79cda881348d88a382f53d
Iryna Howarth <irynahowarth@gmail.com>
2024-02-16 12:18:16 +0000

    [refactor]: add alternative Modal version and first steps to refactor

--- src/components/Recipe/RecipeCard.jsx
+++ src/components/Recipe/RecipeCard.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Modal from "../Modal/Modal";
+import ModalAlt from "../Modal/ModalAlt";
+import * as Dialog from "@radix-ui/react-dialog";
+import { Pencil1Icon, Cross1Icon } from "@radix-ui/react-icons";
 import { ModalDataContext } from "../Modal/ModalDataProvider";
 
 export default function RecipeCard({ recipe }) {
@@ -29,6 +32,25 @@ export default function RecipeCard({ recipe }) {
       >
         +
       </button>
+      <div>
+        <Dialog.Root>
+          <Dialog.Trigger>
+            <Pencil1Icon />
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay className="fixed inset-0 bg-black/50">
+              <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white text-gray-900 rounded-md p-8 shadow w-full max-w-md">
+                <div className="flex justify-between items-center">
+                  <Dialog.Title className="text-xl">Add record</Dialog.Title>
+                  <Dialog.Close className="text-gray-400 hover:text-gray-500">
+                    <Cross1Icon />
+                  </Dialog.Close>
+                </div>
+              </Dialog.Content>
+            </Dialog.Overlay>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </div>
     </li>
   );
 }
