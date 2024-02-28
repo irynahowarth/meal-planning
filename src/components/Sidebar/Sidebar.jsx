293b63bda10e72e6d62fe13523c21d78a1ff5add
Iryna Howarth <irynahowarth@gmail.com>
2024-02-28 10:58:56 +0000

    [refactor]: Sidebar: adapt to use info from RecipeDataProvider for groups and recipeList

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,20 +2,12 @@ import React from "react";
 import RecipeList from "../Recipe/RecipeList";
 import ModeSwitch from "./ModeSwitch";
 import RecipeGroups from "./RecipeGroups";
+import { RecipeDataContext } from "../Recipe/RecipeDataProvider";
 
 export default function Sidebar() {
-  const [groupList, setGroupList] = React.useState([]);
   const [activeGroup, setActiveGroup] = React.useState(1);
-  const [recepieList, setRecepieList] = React.useState([]);
+  const { groupList, recepieList } = React.useContext(RecipeDataContext);
 
-  React.useEffect(() => {
-    fetch("/api/groups")
-      .then((res) => res.json())
-      .then((data) => setGroupList(data.groups));
-    fetch("/api/recipes")
-      .then((res) => res.json())
-      .then((data) => setRecepieList(data.recipes));
-  }, []);
   return (
     <div className="sidebar h-screen relative flex flex-col min-w-[220px] ">
       <header className="border border-gray-200 h-[70px] flex items-center pl-5">
